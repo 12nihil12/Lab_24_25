0135f3f173da0fcc35a9e32954ac0631615452e2
Elias <3l1a5.elias.618@gmail.com>
2024-11-07 15:22:29 +0100

    FIXING LAST ES

--- 28_10/esercizio6.3/main.C
+++ 28_10/esercizio6.3/main.C
@@ -17,13 +17,13 @@ auto * s = new bisezione();
 
 for(int n =1; n<=20; n++){
     try{
-    double zero= s->find_zero(M_PI*n,(n+0.5)*3.14,p,0.001); 
-    cout << setprecision(-log10(s->Getprec())) << zero << endl; 
+    double zero= s->find_zero(M_PI*n + 0.0000001 ,(n+0.5)*M_PI - 0.0000001,p,0.00000001); 
+    cout << "n: " << n << " |" << setprecision(-log10(s->Getprec())) << zero << endl; 
 
 
     }
     catch(...){
-        cout << "Skipping" << n << " iteration" << endl; 
+        cout << "Skipping " << n << " iteration" << endl; 
     }
 }
 
